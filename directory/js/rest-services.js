d6a642773ddf26088fef9941fff5b6e2bd90dcf4
Wonder Fan <wonderfan@ubuntu.(none)>
2014-02-17 18:41:42 -0800

    AngularJs

---
+++ directory/js/rest-services.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.restServices', ['ngResource'])
+    .factory('Employee', ['$resource',
+        function ($resource) {
+            return $resource('http://localhost:3000/employees/:employeeId', {});
+        }])
+
+    .factory('Report', ['$resource',
+        function ($resource) {
+            return $resource('http://localhost:3000/employees/:employeeId/reports', {});
+        }]);
+        
+    
