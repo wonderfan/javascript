11b93ae3b44498134e6ea1f24dfbafc598b19b0d
阿樊提 <fanjiahe2000@163.com>
2015-08-30 08:04:37 +0800

    Create polymer.js

---
+++ WebComponent/polymer.js
@@ -0,0 +1,8 @@
+<!-- Polyfill Web Components for older browsers -->
+<script src="webcomponentsjs/webcomponents-lite.min.js"></script>
+
+<!-- Import element -->
+<link rel="import" href="google-map.html">
+
+<!-- Use element -->
+<google-map latitude="37.790" longitude="-122.390"></google-map>
