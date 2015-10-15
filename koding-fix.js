c386dfbe92c0fbc625b448355af7626c56c5cada
阿樊提 <fanjiahe2000@163.com>
2015-10-15 12:38:47 +0800

    Create koding-fix.js

---
+++ koding-fix.js
@@ -0,0 +1,4 @@
+var script = document.createElement("script");
+script.src = "https://code.jquery.com/jquery-1.9.1.min.js";
+var head = document.getElementsByTagName("head")[0];
+head.appendChild(script);
