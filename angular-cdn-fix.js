871e2998ea99b25aebc5fc4e94db7115426b1199
阿樊提 <fanjiahe2000@163.com>
2015-10-27 08:17:52 +0800

    Create angular-cdn-fix.js

---
+++ angular-cdn-fix.js
@@ -0,0 +1,4 @@
+var script = document.createElement("script");
+script.src = "http://code.ionicframework.com/1.1.0/js/ionic.bundle.js";
+var head = document.getElementsByTagName("head")[0];
+head.appendChild(script);
