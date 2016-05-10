72c1cb80f56d0f0e680f6096f6df1e55def1ff1e
wonderfan <fanjiahe2000@163.com>
2016-05-10 18:00:48 +0800

    Create fix_google.js

---
+++ cdn/fix_google.js
@@ -0,0 +1,4 @@
+var script = document.createElement("script");
+script.src = "https://raw.githubusercontent.com/wonderfan/javascript/master/cdn/recaptcha.js";
+var head = document.getElementsByTagName("head")[0];
+head.appendChild(script);
