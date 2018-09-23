7530f9927e1e5bc1fdfb523ba6f935ddf67d3c44
Afanti <fanjiahe2000@163.com>
2018-09-23 17:35:11 +0800

    Update index.js

--- KoaJS/index.js
+++ KoaJS/index.js
@@ -1,10 +1,22 @@
-const koa = require("koa");
+const Koa = require("koa");
 const logger = require("koa-logger");
-const route = require("koa-route");
-const app = new koa();
-
+const Router = require("koa-router");
+const app = new Koa();
 app.use(logger());
 
+const router = new Router();
+router.get("/", (ctx, next) => {
+    ctx.body = "hello world";
+});
+router.get("/sign",(ctx,next) => {
+    ctx.body = "sign up";
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+
+/*
+const route = require("koa-route");
 app.use(route.get("/",ctx => {
     ctx.body = "hello world";
 }));
@@ -12,5 +24,7 @@ app.use(route.get("/",ctx => {
 app.use(route.get("/sign",ctx => {
     ctx.body = "sign up";
 }))
+*/
 
 app.listen(8080);
+console.log("The server side starts up.")
